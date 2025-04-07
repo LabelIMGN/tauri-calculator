acd0f2c796d3023379b32e4399a8b75947f5a76f
Gabriel Picotte <imgn-studio@Gabriels-MacBook-Pro.local>
2025-04-07 15:30:49 -0400

    FIxed typo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 //import { invoke } from "@tauri-apps/api/core";
 
-const display = document.getElementById("display") as HTMLInputElement
+const display = document.getElementById("display") as HTMLInputElement;
 console.log("Display element:", display);
 
 function appendToDisplay(input: string) :void {
