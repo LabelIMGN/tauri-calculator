baac088c3f3a0ca62f06209b9cd56f983a4d1b47
Gabriel Picotte <imgn-studio@Gabriels-MacBook-Pro.local>
2025-04-07 15:12:25 -0400

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+//import { invoke } from "@tauri-apps/api/core";
+
+const display = document.getElementById("display") as HTMLInputElement
+console.log("Display element:", display);
+
+function appendToDisplay(input: string) :void {
+  display.value += input;
+  console.log("Display element:", display);
+}
