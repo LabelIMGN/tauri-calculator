048204385aac96cec1a6ce49e8f902ccef9a800b
Gabriel Picotte <imgn-studio@Gabriels-MacBook-Pro.local>
2025-04-09 17:10:39 -0400

    Implemented most operators

--- src/main.js
+++ src/main.js
@@ -1,9 +1,38 @@
-//import { invoke } from "@tauri-apps/api/core";
+import { invoke } from "@tauri-apps/api/core";
 
-const display = document.getElementById("display") as HTMLInputElement;
-console.log("Display element:", display);
+const display = document.getElementById("display");
+let isOpenParenthesis = true;
 
-function appendToDisplay(input: string) :void {
-  display.value += input;
-  console.log("Display element:", display);
+window.appendToDisplay = function(input){
+    
+
+  if(input === '()'){
+
+    display.value += isOpenParenthesis ? '(' : ')';
+    isOpenParenthesis = !isOpenParenthesis;
+    return;
+  }
+  
+  // if(input === '+/-'){
+  // }
+  else{
+    display.value += input;
+  }
+
+}
+
+window.clearDisplay = function(){
+
+  display.value = "";
+}
+
+window.calculate = function(){
+
+  let expression = display.value
+    .replace(/×/g, '*')
+    .replace(/÷/g, '/')
+    .replace(/−/g, '-')
+    .replace(/%/g, '/100')
+
+  display.value = eval(expression);
 }
